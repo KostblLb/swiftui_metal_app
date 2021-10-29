53eccf40d31eac39004ee69d1799b04892cea09f
KotblLb <kostya19494@gmail.com>
2021-10-29 21:06:36 +0700

    Initial Commit

---
+++ Shared/ModelViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewerApp.swift
+//  Shared
+//
+//  Created by Konstantin Vysotski on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
